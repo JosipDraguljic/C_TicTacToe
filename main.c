c9bad8cc0b5210d1f9ccaadea800572c6cfb037c
SnowBuddy1997 <jdraguljic@gmail.com>
2021-04-17 00:13:07 +0200

    adding some dummy values

--- main.c
+++ main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char TicTacToe[3][3] = {{'a', 'b', 'c'}, {'a', 'b', 'c'}, {'a', 'b', 'c'}};
+
+    printf("%c", TicTacToe[0][0]);
+    printf("%c", TicTacToe[1][1]);
+
     return 0;
 }
