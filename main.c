b0183ce1242c8a64835bb2d1e9e053df87e60e84
SnowBuddy1997 <jdraguljic@gmail.com>
2021-04-17 00:19:44 +0200

    adding bool to switch between X and O

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<stdbool.h>
 
 #define MAX_ROW 3
 #define MAX_COLUMN 3
 
 int main()
 {
-    char TicTacToe[MAX_ROW][MAX_COLUMN] = {};
+    char TicTacToe[MAX_ROW][MAX_COLUMN] = {}, X = 88, O = 79;
+    bool start = true;
 
     for(int IndexRow = 0; IndexRow < MAX_ROW; IndexRow++)
     {
         for(int IndexColumn = 0; IndexColumn < MAX_COLUMN; IndexColumn++)
         {
+            TicTacToe[IndexRow][IndexColumn] = (start)? X : O;
             printf("| %c |", TicTacToe[IndexRow][IndexColumn]);
+            start = !start;
         }
         printf("\n");
     }
