4bb1ed503b2d4ed09f6c437831fd4f4b351a97f9
SnowBuddy1997 <jdraguljic@gmail.com>
2021-04-17 00:16:29 +0200

    adding nested for loop, getting rid of dummy values

--- main.c
+++ main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define MAX_ROW 3
+#define MAX_COLUMN 3
+
 int main()
 {
-    char TicTacToe[3][3] = {{'a', 'b', 'c'}, {'a', 'b', 'c'}, {'a', 'b', 'c'}};
+    char TicTacToe[MAX_ROW][MAX_COLUMN] = {};
 
-    printf("%c", TicTacToe[0][0]);
-    printf("%c", TicTacToe[1][1]);
+    for(int IndexRow = 0; IndexRow < MAX_ROW; IndexRow++)
+    {
+        for(int IndexColumn = 0; IndexColumn < MAX_COLUMN; IndexColumn++)
+        {
+            printf("| %c |", TicTacToe[IndexRow][IndexColumn]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
